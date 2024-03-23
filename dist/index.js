406cb8b338179c18ea2084b3652ce126f8d0f6ea
MarioLemus <theliarnorland@gmail.com>
2024-03-22 22:48:41 -0600

    first commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// src/index.ts
+function test() {
+  console.log("hello world");
+}
+function test2() {
+  console.log("cuak");
+}
+module.exports = { test, test2 };
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export function test():void {
+    console.log("hello world")
+}
+
+export function test2():void {
+    console.log("cuak")
+}
